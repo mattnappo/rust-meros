24e4a1f1554a8fda1381efd91cffa2bf768731f1
xoreo-arch <mattnappo@gmail.com>
2020-07-11 12:57:03 -0400

    primitives: Implementing File::new().

--- src/primitives/file.rs
+++ src/primitives/file.rs
@@ -1,3 +1,6 @@
+use std::fs::File;
+use std::io::prelude::*;
+
 use crate::db::shard_db;
 
 /// The structure used for the identification of a file on the meros
@@ -9,17 +12,18 @@ type FileID = Hash;
 /// content of the file is not included in this structure. The actual
 /// data of the file is stored at the nodes described in the `File`'s
 /// `shard_db` field.
-pub struct File {
-    pub filename: str,
+pub struct File<'a> {
+    pub filename: &'a str,
     pub fileID: FileID,
-    shard_db: shard_db::ShardDB,
+    shard_db: database::Database<Shard>,
 }
 
-impl File {
+impl File<'a> {
     /// Abstraction method to generate the metadata of a `File`.
     /// This method does not distribute a file over the meros network.
-    fn new_file_from_disk(filepath: &str) {}
-    fn calc_file_id(&self) ->
+    /// However, it does prepare the file for sharding by pre-calculating
+    /// the shards and assigning them to null nodes (temporarily).
+    fn new(path: &str) -> Self {}
 }
 
 impl Hashable for File {
