4934aad3a75c5f55b0ef1a845a96468816df5100
xoreo-arch <mattnappo@gmail.com>
2020-07-11 12:45:03 -0400

    primitives: Started implementing the core types. Worked on implemented `File` hashing.

--- src/primitives/file.rs
+++ src/primitives/file.rs
@@ -1,11 +1,27 @@
 use crate::db::shard_db;
 
+/// The structure used for the identification of a file on the meros
+/// network. It is a hash calculated in a specific way, as described in
+/// the `File` implementation.
+type FileID = Hash;
+
 /// The structure representing a file in the main meros dht. The actual
 /// content of the file is not included in this structure. The actual
 /// data of the file is stored at the nodes described in the `File`'s
 /// `shard_db` field.
 pub struct File {
     pub filename: str,
-    pub fileID: Vec<u8>,
+    pub fileID: FileID,
     shard_db: shard_db::ShardDB,
+}
+
+impl File {
+    /// Abstraction method to generate the metadata of a `File`.
+    /// This method does not distribute a file over the meros network.
+    fn new_file_from_disk(filepath: &str) {}
+    fn calc_file_id(&self) ->
+}
+
+impl Hashable for File {
+    fn hash(&self) -> Hash {}
 }
--- src/primitives/mod.rs
+++ src/primitives/mod.rs
@@ -1,1 +1,12 @@
 pub mod file;
+
+/// The default size of hashes
+const HASH_SIZE: u8; 32;
+
+/// A type alias for Hashes to be used in the `primitives` module.
+type Hash = [u8; HASH_SIZE];
+
+/// A trait given to types that are able to be hashed.
+trait Hashable {
+    fn hash(&self) -> Vec<u8>;
+}
