fbc419c74fa5182e448a520bcf635f022a66819d
xoreo <mattnappo@gmail.com>
2020-07-17 09:18:24 -0400

    crypto: Removed debugging code.

--- src/crypto/encryption.rs
+++ src/crypto/encryption.rs
@@ -100,12 +100,7 @@ fn load_keypair(name: &str) -> Result<Keypair, CryptoError> {
 pub trait CanEncrypt: CanSerialize {
     type D: CanEncrypt;
 
-    // fn encrypt(&self, key: PublicKey) -> Result<Vec<u8>, CryptoError>;
-    fn encrypt(
-        &self,
-        key: PublicKey,
-        secret: SecretKey,
-    ) -> Result<Vec<u8>, CryptoError>;
+    fn encrypt(&self, key: PublicKey) -> Result<Vec<u8>, CryptoError>;
 
     fn decrypt(
         bytes: Vec<u8>,
@@ -116,24 +111,14 @@ pub trait CanEncrypt: CanSerialize {
 impl CanEncrypt for File {
     type D = Self;
 
-    fn encrypt(
-        &self,
-        key: PublicKey,
-        secret: SecretKey,
-    ) -> Result<Vec<u8>, CryptoError> {
+    fn encrypt(&self, key: PublicKey) -> Result<Vec<u8>, CryptoError> {
         let mut csprng = rand::thread_rng();
         let bytes = self
             .to_bytes()
             .map_err(|e| CryptoError::SerializationError(e))?;
 
-        let encrypted = encrypt(&key, &bytes, &mut csprng)
-            .map_err(|e| CryptoError::EncryptionError(e))?; // return this
-        let decrypted = decrypt(&secret, &encrypted).unwrap();
-        println!(
-            "decrypted it inside the encrypt() method: {:?}",
-            File::from_bytes(decrypted).unwrap()
-        );
-        Ok(Vec::new())
+        encrypt(&key, &bytes, &mut csprng)
+            .map_err(|e| CryptoError::EncryptionError(e))
     }
 
     fn decrypt(
@@ -143,7 +128,6 @@ impl CanEncrypt for File {
         let decrypted = decrypt(&key, &bytes)
             .map_err(|e| CryptoError::EncryptionError(e))?;
 
-        println!("THIS IS WHERE THE PROBLEM IS HAPPENING");
         <Self::D as CanSerialize>::from_bytes(decrypted)
             .map_err(|e| CryptoError::SerializationError(e))
     }
@@ -160,18 +144,15 @@ mod tests {
     fn test_encrypt_file() {
         let file = File::new(Path::new("testfile.txt")).unwrap();
         let keypair = gen_keypair("testkey", false).unwrap();
-
-        let encrypted = file.encrypt(keypair.1, keypair.0).unwrap();
-        println!("encrypted: {:?}", encrypted);
+        file.encrypt(keypair.1).unwrap();
     }
 
     #[test]
     fn test_decrypt_file() {
         let file = File::new(Path::new("testfile.txt")).unwrap();
         let keypair = gen_keypair("testkey", false).unwrap();
-        let encrypted = file.encrypt(keypair.1, keypair.0).unwrap();
-
-        let decrypted = File::decrypt(encrypted, keypair.0).unwrap();
+        let encrypted = file.encrypt(keypair.1).unwrap();
+        File::decrypt(encrypted, keypair.0).unwrap();
     }
 
     #[test]
@@ -187,58 +168,4 @@ mod tests {
 
     #[test]
     fn test_load_keypair() {}
-
-    #[test]
-    fn test_basics() {
-        use super::super::hash::*;
-        use ecies_ed25519::{
-            decrypt, encrypt, generate_keypair, PublicKey, SecretKey,
-        };
-        use serde::{Deserialize, Serialize};
-
-        #[derive(Serialize, Deserialize, Debug)]
-        struct B {
-            r1: u128,
-            r2: String,
-            r3: Hash,
-        }
-
-        impl CanSerialize for B {
-            type S = Self;
-            fn to_bytes(&self) -> bincode::Result<Vec<u8>> {
-                bincode::serialize(self)
-            }
-            fn from_bytes(bytes: Vec<u8>) -> bincode::Result<Self::S> {
-                bincode::deserialize(&bytes[..])
-            }
-        }
-
-        let b = B {
-            r1: 123087,
-            r2: std::string::String::from("random str"),
-            r3: hash_bytes(vec![1, 2, 3, 4, 5]),
-        };
-        println!("B: {:?}", b);
-        let serialized = b.to_bytes().unwrap();
-
-        // Encryption time
-
-        let mut csprng = rand::thread_rng();
-        let (secret, public) =
-            ecies_ed25519::generate_keypair(&mut csprng);
-
-        // Encrypt the message with the public key such that only
-        // the holder of the secret key can decrypt.
-        let encrypted =
-            ecies_ed25519::encrypt(&public, &serialized, &mut csprng)
-                .unwrap();
-
-        // Decrypt the message with the secret key
-        let decrypted =
-            ecies_ed25519::decrypt(&secret, &encrypted).unwrap();
-        println!("\n\nDECRYPTED: {:?}\n\n", decrypted);
-
-        let reconstructed = B::from_bytes(decrypted).unwrap();
-        println!("RECONSTRUCTED: {:?}", reconstructed);
-    }
 }
