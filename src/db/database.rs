767bc35a94758e1111e9df05aef3002d22cdde60
xoreo <mattnappo@gmail.com>
2020-08-10 22:42:01 -0400

    database: Writing the database constructor.

--- src/db/database.rs
+++ src/db/database.rs
@@ -1,3 +1,6 @@
+use super::*;
+use sled;
+
 /// The different types of data that a `Database` can hold.
 pub enum DBType {
     File,
@@ -7,20 +10,34 @@ pub enum DBType {
 /// The main database structure used to store the metadata and node
 /// information for all of the files on the network, and the shards
 /// on each local node.
-pub struct Database<K: super::IsKey, V: super::IsValue> {
-    database: Vec<u8>, // temp
+pub struct Database {
+    name: String,
+    database: sled::Db,
     db_type: DBType,
-    default_key: K,
-    default_value: V,
 }
 
-impl<K: super::IsKey, V: super::IsValue> Database<K, V> {
-    fn new(db_type: DBType, dk: K, dv: V) -> Self {
-        Self {
-            database: Vec::new(),
+impl Database {
+    /// Create and return a new database if it does not already
+    /// exist.
+    fn new(name: &str, db_type: DBType) -> Result<Self, DatabaseError> {
+        // Determine the string location for the database
+        let loc = match &db_type {
+            File => "file",
+            Shard => "shard",
+        };
+
+        // Create the new database
+        let database =
+            sled::open(format!("{}/{}/{}", super::ROOT, loc, name))
+                .map_err(|e| super::DatabaseError::Internal(e))?;
+
+        Ok(Self {
+            name: name.to_string(),
+            database,
             db_type,
-            default_key: dk,
-            default_value: dv,
-        }
+        })
     }
+
+    // Insert a record into the database.
+    // fn insert(&mut self, key: K, val: V) -> DatabaseError {}
 }
--- src/db/mod.rs
+++ src/db/mod.rs
@@ -2,3 +2,10 @@ pub mod database;
 
 pub trait IsKey {}
 pub trait IsValue {}
+
+/// All of the errors that can be thrown in the database module.
+pub enum DatabaseError {
+    Internal(sled::Error),
+}
+
+const ROOT: &str = "./data/db";
